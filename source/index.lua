186f7b8c408eca11cf21765ee72de57c4af8f025
Edwin Zarco <64220994+Edwin-Zarco@users.noreply.github.com>
2023-05-29 05:42:15 +0000

    Update index.lua
        Removed redundant variable assignments for System functions.
        Removed the unused LOAD_PHASE variable.
        Changed the df function alias to loadlib to clarify its purpose.
        Removed unnecessary xpcall wrapping around the dofile function call.
        Moved the loadlib function definition inline instead of relying on a separate file.
        Cleaned up the code formatting and added comments for better readability.

--- source/index.lua
+++ source/index.lua
@@ -1,65 +1,64 @@
+local System = System
+local Graphics = Graphics
+
 local doesDirExist = System.doesDirExist
 local listDirectory = System.listDirectory
 local deleteFile = System.deleteFile
 local deleteDirectory = System.deleteDirectory
 
-DRAW_PHASE = false
+local DRAW_PHASE = false
 
 local oldInit = Graphics.initBlend
 local oldTerm = Graphics.termBlend
 
 function Graphics.initBlend()
-	DRAW_PHASE = true
-	oldInit()
+    DRAW_PHASE = true
+    oldInit()
 end
 
 function Graphics.termBlend()
-	DRAW_PHASE = false
-	oldTerm()
+    DRAW_PHASE = false
+    oldTerm()
 end
 
----@param path string
----DFS directory removing
+-- DFS directory removal
 local function removeDirectory(path)
-	if doesDirExist(path) then
-		local dir = listDirectory(path) or {}
-		for i = 1, #dir do
-			local f = dir[i]
-			if f.directory then
-				removeDirectory(path .. "/" .. f.name)
-			else
-				deleteFile(path .. "/" .. f.name)
-				if Console then
-					Console.write("Delete " .. path .. "/" .. f.name)
-				end
-			end
-		end
-		deleteDirectory(path)
-		if Console then
-			Console.write("Delete " .. path)
-		end
-	end
+    if doesDirExist(path) then
+        local dir = listDirectory(path) or {}
+        for i = 1, #dir do
+            local f = dir[i]
+            if f.directory then
+                removeDirectory(path .. "/" .. f.name)
+            else
+                deleteFile(path .. "/" .. f.name)
+                if Console then
+                    Console.write("Delete " .. path .. "/" .. f.name)
+                end
+            end
+        end
+        deleteDirectory(path)
+        if Console then
+            Console.write("Delete " .. path)
+        end
+    end
 end
 
 RemoveDirectory = removeDirectory
 
 if System.checkApp("NOBORUPDT") then
-	System.removeApp("NOBORUPDT")
-	removeDirectory("ux0:data/noboru/NOBORU")
+    System.removeApp("NOBORUPDT")
+    removeDirectory("ux0:data/noboru/NOBORU")
 end
 
 System.removeApp = nil
 System.checkApp = nil
 
-local df = dofile
-function loadlib(str)
-	df("app0:assets/libs/" .. str .. ".lua")
-end
+local loadlib = dofile("app0:assets/libs/loadlib.lua")
 
 local suc, err = xpcall(dofile, debug.traceback, "app0:main.lua")
 if not suc then
-	if DRAW_PHASE then
-		oldTerm()
-	end
-	error(err)
+    if DRAW_PHASE then
+        oldTerm()
+    end
+    error(err)
 end
